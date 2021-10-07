ec7a820680172449f03e85b73f77cc690e10dcb0
Menji <jennifer.menjivar18@gmail.com>
2021-10-07 17:07:05 -0400

    commented out lines

--- src/components/Photolist.js
+++ src/components/Photolist.js
@@ -122,20 +122,20 @@ function Photolist({ category }) {
 
   const currentPhotos = photos.filter((photo) => photo.category === category);
 
-  return (
-    <div>
-      <div className="flex-row">
-        {currentPhotos.map((image, i) => (
-          <img
-            src={require(`../assets/small/${category}/${i}.jpg`).default}
-            alt={image.name}
-            className="img-thumbnail mx-1"
-            key={image.name}
-          />
-        ))}
-      </div>
-    </div>
-  );
+  // return (
+  //   <div>
+  //     <div className="flex-row">
+  //       {currentPhotos.map((image, i) => (
+  //         <img
+  //           src={require(`../assets/small/${category}/${i}.jpg`).default}
+  //           alt={image.name}
+  //           className="img-thumbnail mx-1"
+  //           key={image.name}
+  //         />
+  //       ))}
+  //     </div>
+  //   </div>
+  // );
 }
 
 export default Photolist;
--- src/components/tbd.js
+++ src/components/tbd.js
@@ -1,50 +1,50 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-function Photolist({ category }) {
-  const [projects] = useState([
-    {
-      name: "Castle Cottage or Cave",
-      image: "commercial",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
-    },
-    {
-      name: "Weather Dashboard",
-      image: "commercial",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
-    },
-    {
-      name: "Tech Blog",
-      image: "commercial",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
-    },
+// function Photolist({ category }) {
+//   const [projects] = useState([
+//     {
+//       name: "Castle Cottage or Cave",
+//       image: "commercial",
+//       description:
+//         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
+//     },
+//     {
+//       name: "Weather Dashboard",
+//       image: "commercial",
+//       description:
+//         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
+//     },
+//     {
+//       name: "Tech Blog",
+//       image: "commercial",
+//       description:
+//         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
+//     },
 
-    {
-      name: "Fruit bowl",
-      image: "food",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
-    },
-  ]);
+//     {
+//       name: "Fruit bowl",
+//       image: "food",
+//       description:
+//         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
+//     },
+//   ]);
 
-  const currentPhotos = photos.filter((photo) => photo.image === image);
+//   const currentPhotos = photos.filter((photo) => photo.image === image);
 
-  return (
-    <div>
-      <div className="flex-row">
-        {currentPhotos.map((image, i) => (
-          <img
-            src={require(`../../assets/small/${image}/${i}.jpg`).default}
-            alt={image.name}
-            className="img-thumbnail mx-1"
-            key={image.name}
-          />
-        ))}
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <div className="flex-row">
+//         {currentPhotos.map((image, i) => (
+//           <img
+//             src={require(`../../assets/small/${image}/${i}.jpg`).default}
+//             alt={image.name}
+//             className="img-thumbnail mx-1"
+//             key={image.name}
+//           />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
 
-export default Photolist;
+// export default Photolist;
